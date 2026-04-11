c531e36e23ee13fa7c12bfc75925991e73e92cf9
Zene <database@zene.uk>
2026-04-11 17:05:35 +0100

    Added gate struct

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -2,3 +2,83 @@
 
 mod midi;
 pub use crate::midi::*;
+
+pub enum InputMode
+{
+    TakeFirst,
+    TakeLast,
+    Ignore
+}
+
+pub struct PanelState
+{
+    pub channels: u8,
+    pub voices: u8,
+    pub octave: i8,
+    pub pitch_bend: u8,
+    pub stack: bool,
+    pub modulation: bool,
+    pub input: InputMode
+}
+
+pub struct Externals
+{
+    pub set_gate: fn(Gate),
+    pub set_note: fn(usize, u8),
+    pub set_vel: fn(usize, u8),
+    pub set_mod: fn(u16),
+    
+    pub get_time: fn() -> usize
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Gate
+{
+    value: u8
+}
+
+impl Gate
+{
+    pub fn set(self, bit: u8, value: bool) -> Gate
+    {
+        return match value
+        {
+            true => self.on(bit),
+            false => self.off(bit)
+        };
+    }
+    pub fn on(self, bit: u8) -> Gate
+    {
+        return Gate { value: self.value | 1 << bit };
+    }
+    pub fn off(self, bit: u8) -> Gate
+    {
+        return Gate { value: self.value & !(1 << bit) };
+    }
+    pub fn get(&self, bit: u8) -> bool
+    {
+        return (self.value & 1 << bit) > 0;
+    }
+    
+    pub const fn zero() -> Gate
+    {
+        return Gate { value: 0 };
+    }
+    pub const fn all_on() -> Gate
+    {
+        return Gate { value: 0b00011111 };
+    }
+    pub const fn new(value: u8) -> Gate
+    {
+        return Gate { value };
+    }
+    
+    pub const fn invert(self) -> Gate
+    {
+        return Gate { value: !self.value };
+    }
+    pub const fn mask(self, mask: Gate) -> Gate
+    {
+        return Gate { value: self.value & mask.value };
+    }
+}
